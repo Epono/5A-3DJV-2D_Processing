8071f1ad87ded2e49b64289692d7f7efac40d976
SebastienGlr <sebastien.gaulier@gmail.com>
2015-12-01 00:19:49 +0100

    Fix de l'algo pour bien afficher

--- 2D_Processing/src/Triangulation2D_qcq.cpp
+++ 2D_Processing/src/Triangulation2D_qcq.cpp
@@ -3,33 +3,45 @@
 
 #define LOG_Triangulation2D_qcq 0
 
-bool myComparator(Point* a, Point* b) {
-	return (a->getX() != b->getX() ? a->getX() < b->getX() : a->getY() > b->getY());
+bool myComparator(Point* a, Point* b) 
+{// Abscisse croissante ou ou ordonnee croissante si abscisses egales
+	return (a->getX() < b->getX() || (a->getX() == b->getX() && a->getY() < b->getY()));
 }
 
-Triangulation2D_qcq::Triangulation2D_qcq(std::vector<Point*> points) {
-	_points = points;
-}
+Triangulation2D_qcq::Triangulation2D_qcq(std::vector<Point*> points)
+	: TriangleStrip(points)
+{}
 
-void Triangulation2D_qcq::computeTriangulation() {
-	if(_points.empty()) {
+void Triangulation2D_qcq::computeTriangulation() 
+{
+	// 3 points mini pour trianguler
+	if(_points.size() < 3) 
+	{
+		std::cout << "Nuage de points vide" << std::endl;
 		return;
 	}
-
 	// On trie les points par x croissant (y croissant si égalité (décroissant pour nous ? vu quec'ets inverse))
 	std::sort(_points.begin(), _points.end(), myComparator); // 12 32 45 71(26 33 53 80)
 
 	// Initialisation avec 2-triangulation initial (seulement si y'a des points aliggnés ? wtf)
 	Point* firstPoint = _points.at(0);
 	std::vector<Point*> minPoints;
-	int i;
-	for(i = 0; i < _points.size(); ++i) {
-		if(_points.at(i)->getX() == firstPoint->getX()) {
+	
+	for(int i = 0; i < _points.size(); ++i) 
+	{
+		if(_points.at(i)->getX() == firstPoint->getX()) 
+		{
 			minPoints.push_back(_points.at(i));
 		}
-		else {
+		else 
+		{
 			break;
 		}
+	}
+	if (minPoints.size() == 1)
+	{
+		// Si pas assez de points on rajoute le suivant
+		minPoints.push_back(_points.at(1));
 	}
 
 #if LOG_Triangulation2D_qcq
@@ -40,19 +52,15 @@ void Triangulation2D_qcq::computeTriangulation() {
 	}
 #endif
 
+	int i = minPoints.size();
 	// Si tous les points sont colinéaires
-	if(i >= _points.size()) {
+	if(minPoints.size() == _points.size()) 
 		return;
-	}
 
+	// Create start edges
 	std::vector<Line*> edges;
-	Point* firstOtherPoint = _points.at(i);
-
-	if(minPoints.size() > 1) {
-		for(int j = 0; j < minPoints.size() - 1; ++j) {
-			edges.push_back(new Line(minPoints.at(j), minPoints.at(j + 1)));
-		}
-	}
+	for(int j = 0; j < minPoints.size() - 1; ++j) 
+		edges.push_back(new Line(minPoints.at(j), minPoints.at(j + 1)));
 
 #if LOG_Triangulation2D_qcq
 	std::cout << "Liste des 1eres aretes : " << std::endl;
@@ -61,8 +69,10 @@ void Triangulation2D_qcq::computeTriangulation() {
 	}
 #endif
 
+	Point* firstOtherPoint = _points.at(i);
 	std::vector<Line*> edgesToAdd;
-	for(auto& line : edges) {
+	for(auto& line : edges) 
+	{
 		Line* lineTemp1 = new Line(line->getStartPoint(), firstOtherPoint);
 		Line* lineTemp2 = new Line(line->getEndPoint(), firstOtherPoint);
 		Triangle* t = new Triangle(line->getStartPoint(), line->getEndPoint(), firstOtherPoint); // Si on le met sous les vectors ça bug u_u
@@ -76,13 +86,16 @@ void Triangulation2D_qcq::computeTriangulation() {
 
 	// Itération
 	// Pour chaque point restant 
-	for(i = i; i < _points.size(); ++i) {
+	for(i = i; i < _points.size(); ++i) 
+	{
 		Point* tempPoint = _points.at(i);
 		std::vector<Line*> edgesToAdd2;
 		// Pour chaque arete existant visible par le point
-		for(auto& line : edges) {
+		for(auto& line : edges)
+		{
 			// Manque la condition
-			if(true) {
+			if(true) 
+			{
 				Line* lineTemp1 = new Line(line->getStartPoint(), tempPoint);
 				Line* lineTemp2 = new Line(line->getEndPoint(), tempPoint);
 				Triangle* t = new Triangle(line->getStartPoint(), line->getEndPoint(), tempPoint); // Si on le met sous les vectors ça bug u_u
